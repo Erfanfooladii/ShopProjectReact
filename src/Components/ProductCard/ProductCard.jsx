d96bae70f63d5e6dba5483bb82a2c39c55947930
Erfanfooladii <arfannnfooladi@gmail.com>
2024-12-05 19:51:58 +0330

    feat: Add a files for product list

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
 import "./ProductCard.css"
+import linkStar from "https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png"
 const ProductCard=({productImage,category,productName,productPrice})=>{
     return(
         <>
@@ -9,8 +10,15 @@ const ProductCard=({productImage,category,productName,productPrice})=>{
                     <h2>{productName}</h2>
                     <span>{productPrice}</span>
                     <div className="stars">
-                        <img src="" alt="star icon" />
+                        <img src={linkStar} alt="star icon" />
+                        <img src={linkStar} alt="star icon" />
+                        <img src={linkStar} alt="star icon" />
                     </div>
+                </div>
+                <div className="content-btn">
+                    <button className="btn-product">
+                        Add To Cart
+                    </button>
                 </div>
             </div>
         </>
