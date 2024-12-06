77910f2674d42ec043ff55f6da999c3ff524cddc
Erfanfooladii <arfannnfooladi@gmail.com>
2024-12-06 12:43:58 +0330

    docs: changes for styles

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,28 +1,28 @@
-import "./ProductCard.css"
-import linkStar from "https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png"
-const ProductCard=({productImage,category,productName,productPrice})=>{
-    return(
-        <>
-            <div className="container-product">
-                <img src={productImage} alt={productImage} />
-                <div className="body-product">
-                    <h1>{category}</h1>
-                    <h2>{productName}</h2>
-                    <span>{productPrice}</span>
-                    <div className="stars">
-                        <img src={linkStar} alt="star icon" />
-                        <img src={linkStar} alt="star icon" />
-                        <img src={linkStar} alt="star icon" />
-                    </div>
-                </div>
-                <div className="content-btn">
-                    <button className="btn-product">
-                        Add To Cart
-                    </button>
+import "./ProductCard.css";
+
+const ProductCard = ({ productImage, category, productName, productPrice }) => {
+    const starImage = "https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png";
+
+    return (
+        <div className="container-product">
+            <img src={productImage} alt={`${productName} image`} />
+            <div className="body-product">
+                <h1 className="category">{category}</h1>
+                <h2 className="name">{productName}</h2>
+                <span className="price">{productPrice} <small>$99</small></span>
+                <div className="stars">
+                    <img src={starImage} alt="icon star" />
+                    <img src={starImage} alt="icon star" />
+                    <img src={starImage} alt="icon star" />
                 </div>
             </div>
-        </>
-    )
-}
+            <div className="content-btn">
+                <button className="btn-product">
+                    Add To Cart
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default ProductCard;
