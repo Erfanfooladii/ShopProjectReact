fe09f77406ff58c8eec49b0f00ceade0a8dbfb5e
Erfanfooladii <arfannnfooladi@gmail.com>
2024-12-08 22:44:52 +0330

    docs: create category component

---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,11 @@
+import InputCheckbox from "../Inputs/inputCheckboxs/InputCheckbox";
+const Category=(params)=> {
+    return(
+        <div>
+            <InputCheckbox
+                name="erfan"
+            />
+        </div>
+    )
+};
+export default Category;
