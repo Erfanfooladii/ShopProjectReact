30c0ac17b4efaeb4e5415932fa18a4213ae6c547
Erfanfooladii <arfannnfooladi@gmail.com>
2024-12-17 09:36:48 +0330

    feat: change get api

--- src/Components/Pages/Prducts/ProductPage.jsx
+++ src/Components/Pages/Prducts/ProductPage.jsx
@@ -6,40 +6,11 @@ import { CategoryContext } from "../../../contexts/categoryContext"
 import Pagination from "../../Pagination/Pagination"
 import { fetchProductApi } from "../../../utils/fetchProducts"
 const ProductPage=()=>{
-
-    const [data,setData]=useState([])
-    const [isLoading,setIsLoading]=useState(undefined)
-    const [error,setError]=useState(undefined)
-    const {selectValueCategory}=useContext(CategoryContext)
-        
-        useEffect(() => {
-            const getApi=async ()=>{
-                setIsLoading(true)
-                try {
-                    const data=await fetchProductApi({category:[...selectValueCategory]})
-                    setData(data)
-                } catch (error) {
-                    setError(error.message)
-                } finally{
-                    setIsLoading(false)
-                }
-            }
-            getApi()
-            document.title= "Procuts page"
-        }, [selectValueCategory]);
-        console.log([...selectValueCategory]);
-        
-    if (isLoading) {
-        return <div>Loading data...</div>
-    }
-    if (error) {
-        return <div>Error: {error} </div>
-    }    
     return(
         <div className="container-product-main">
             <div className="body-product">
                 <Category/>
-                <ProductList data={data}/>
+                <ProductList/>
             </div>
             <div className="footer-product">
                 <Pagination/>
--- src/Components/product-list/ProductList.jsx
+++ src/Components/product-list/ProductList.jsx
@@ -1,7 +1,38 @@
 import "./ProductList.css"
 import ProductCard from "../ProductCard/ProductCard";
+import { CategoryContext } from "../../contexts/categoryContext";
+import { useContext, useEffect, useState } from "react";
+import { fetchProductApi } from "../../utils/fetchProducts";
 
-const ProdcutList = ({data}) => {
+const ProdcutList = () => {
+    const [data,setData]=useState([])
+    const [isLoading,setIsLoading]=useState(undefined)
+    const [error,setError]=useState(undefined)
+    const {selectValueCategory}=useContext(CategoryContext)
+        
+        useEffect(() => {
+            const getApi=async ()=>{
+                setIsLoading(true)
+                try {
+                    const data=await fetchProductApi({category:[...selectValueCategory]})
+                    setData(data)
+                } catch (error) {
+                    setError(error.message)
+                } finally{
+                    setIsLoading(false)
+                }
+            }
+            getApi()
+            document.title= "Procuts page"
+        }, [selectValueCategory]);
+        console.log([...selectValueCategory]);
+        
+        if (isLoading) {
+            return <div>Loading data...</div>
+        }
+        if (error) {
+            return <div>Error: {error} </div>
+        }
     return (
         <div className='container-product-list' >
             {
