39d4ef423238b44589d9aa3a082371f8979f91e9
Erfanfooladii <arfannnfooladi@gmail.com>
2024-12-19 09:22:22 +0330

    docs: create Showpage component

--- src/Components/Pages/Prducts/ProductPage.jsx
+++ src/Components/Pages/Prducts/ProductPage.jsx
@@ -1,16 +1,17 @@
-import { useState,useEffect, useContext } from "react"
 import "./ProductPage.css"
 import ProductList from "../../product-list/ProductList"
 import Category from "../../Category/Category"
-import { CategoryContext } from "../../../contexts/categoryContext"
 import Pagination from "../../Pagination/Pagination"
-import { fetchProductApi } from "../../../utils/fetchProducts"
+import ShowPage from "../../Show/ShowPage"
 const ProductPage=()=>{
     return(
         <div className="container-product-main">
             <div className="body-product">
                 <Category/>
-                <ProductList/>
+                <div className="">
+                    <ShowPage/>
+                    <ProductList/>
+                </div>
             </div>
             <div className="footer-product">
                 <Pagination/>
