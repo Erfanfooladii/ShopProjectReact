d6f36c73ceb38863254114bee6df65ba06383c90
Erfanfooladii <arfannnfooladi@gmail.com>
2024-12-21 18:52:53 +0330

    docs: change pagitation componenet

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,17 +1,26 @@
 import { useContext } from "react";
-import "./Pagination.css"
+import "./Pagination.css";
 import { PaginationContext } from "../../contexts/paginationContext";
-const Pagination=() => {
-    const listNumbers10=[1,2,3,4,5,6,7,8,9,10]
-    const {setSelectPagination}=useContext(PaginationContext)
-    return (
-        <div className="container-pagination">
-            {
-                listNumbers10.map((item,index)=>{
-                    return <button key={index} onClick={()=>{setSelectPagination(item)}} >{item}</button>
-                })
-            }
-        </div>
-    )
+
+const Pagination = ({ totalPages }) => {
+  const { setSelectPagination, selectPagination } = useContext(PaginationContext);
+
+  return (
+    <div className="container-pagination">
+      {
+        Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            className={`${selectPagination === index ? 'active' : ''}`}
+            onClick={() => { console.log(index)
+             setSelectPagination(index+1); }}
+          >
+            {index + 1}
+          </button>
+        ))
+      }
+    </div>
+  );
 };
+
 export default Pagination;
