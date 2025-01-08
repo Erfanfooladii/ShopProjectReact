c1cb6959d8bb4d562edaf4605b968646a718e371
Erfanfooladii <arfannnfooladi@gmail.com>
2025-01-08 17:39:15 +0330

    docs: creating route for single product

--- src/Layouts/MainLayouts/index.jsx
+++ src/Layouts/MainLayouts/index.jsx
@@ -4,6 +4,8 @@ import { GetApiProvider } from '../../Contexts/getApiContext'
 import { SearchProvider } from '../../Contexts/searchContext'
 import Header from '../Header'
 import HomePage from '../../Pages/Home'
+import ProductPage from '../../Pages/ProductPage'
+import Footer from '../Footer'
 
 function MainLayouts() {
   return (
@@ -11,8 +13,10 @@ function MainLayouts() {
         <SearchProvider>
           <Header/>
           <Routes>
-            <Route path='/' element={<HomePage/>} />
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/:id' element={<ProductPage/>} />
           </Routes>
+          <Footer/>
         </SearchProvider>
       </GetApiProvider>
   )
