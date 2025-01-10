c2b066190687480ba219720924a24dfbeaf144b4
Erfanfooladii <arfannnfooladi@gmail.com>
2025-01-10 22:42:11 +0330

    docs: creating a new component for the footer

---
+++ src/Layouts/Footer/Components/FooterBottom/index.jsx
@@ -0,0 +1,10 @@
+const FooterBottom = () => {
+    return(
+        <div className="footer__bottom">
+            <div className="footer__bottom-containder __global-container">
+                <p>Copyright  2025 All Rights Reserved</p>
+            </div>
+        </div>
+    )
+};
+export default FooterBottom;
