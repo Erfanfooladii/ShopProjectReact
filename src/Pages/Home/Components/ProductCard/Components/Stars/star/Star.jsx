c8f605d5f7aed12c8eba5e37ce59f68b2b7e6278
Erfanfooladii <arfannnfooladi@gmail.com>
2025-01-23 17:38:14 +0330

    docs: remove name files

--- src/Pages/Home/Components/ProductCard/Components/Stars/star/Star.jsx
+++
@@ -1,8 +0,0 @@
-import "./Star.css"
-function Star() {
-    const starImage = "https://seeklogo.com/images/R/red-star-logo-2D3327A276-seeklogo.com.png";
-    return(
-        <img className="star" src={starImage} alt="icon star" />
-    )
-};
-export default Star;
