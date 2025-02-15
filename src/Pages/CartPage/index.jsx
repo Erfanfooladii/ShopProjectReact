7c7494b63ff323985b7780a89b60b3cc743263d9
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-02-15 16:56:30 +0330

    docs: api get cart list

--- src/Pages/CartPage/index.jsx
+++ src/Pages/CartPage/index.jsx
@@ -1,45 +1,30 @@
-import { getProductId } from '../../utils/fetchProducts';
 import './style.css';
-import { useEffect, useState } from 'react';
-const CartPage = () => {
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(undefined);
+import { useEffect } from 'react';
+import ListItem from './Components/ListItem';
 
-  const getApi = async (id) => {
-    setIsLoading(true);
-    try {
-      const data = await getProductId(id);
-      setData(data);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+const CartPage = () => {
+  const productCart = JSON.parse(localStorage.getItem('cart'));
+  const listIdCart = productCart.map((item) => item._id);
   useEffect(() => {
-    getApi();
     document.title = 'Cart';
   }, []);
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
   return (
     <div className="cart __global-container">
-      <h1 className="cart__title">Cart list</h1>
       <div className="cart__content">
-        <div className="cart__total">
-          <h2 className="cart__total--price">222$</h2>
-          <h2 className="cart__total--product">23</h2>
+        <div className="cart__details">
+          <h1 className="cart__title">Cart list</h1>
+          <h2 className="cart__total--price">Total price: 222$</h2>
+          <h2 className="cart__total--product">Total products: 23</h2>
+          <button className="cart__button-all-remove">Remove All</button>
         </div>
         <ul className="cart__list">
-          <li className="cart__list--item">
-            <h1 className="cart__list-name">{data?.name}</h1>
-            <div className=""></div>
-          </li>
+          {listIdCart.length > 0 ? (
+            listIdCart.map((id) => {
+              return <ListItem key={id} id={id} />;
+            })
+          ) : (
+            <h2 className="cart_title-empty">Shopping cart is empty.</h2>
+          )}
         </ul>
       </div>
     </div>
