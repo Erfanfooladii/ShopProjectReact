5c87b90e78d4fe344d7739c8b6a9880716f2f838
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-02-17 23:14:55 +0330

    docs: create loading image

--- src/Pages/ProductPage/Components/ItmeProduct/Components/ImageItem/index.jsx
+++ src/Pages/ProductPage/Components/ItmeProduct/Components/ImageItem/index.jsx
@@ -1,18 +1,31 @@
 import PropTypes from 'prop-types';
 import './style.css';
+import { useEffect, useState } from 'react';
+import Spinner from '../../../../../../Components/Spinner';
 
 const ImageItem = ({ data, image }) => {
   const { images = [], name } = data;
   const imageIndex = image ? 0 : 1;
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-  if (imageIndex < images.length) {
+  useEffect(() => {
+    const img = new Image();
+    img.src = image;
+    img.onload = () => setImageLoaded(true);
+  }, [image]);
+
+  if (images.length > 0 && imageIndex < images.length) {
     return (
       <div className="product__item-image">
-        <img
-          src={images[imageIndex]}
-          className="product__item-img"
-          alt={name}
-        />
+        <div className="product__item--loaded">
+          {!imageLoaded && <Spinner size={60} className="loading__image" />}
+          <img
+            onLoad={() => setImageLoaded(true)}
+            src={images[imageIndex]}
+            className="product__item-img"
+            alt={name}
+          />
+        </div>
       </div>
     );
   }
@@ -25,7 +38,7 @@ ImageItem.propTypes = {
     images: PropTypes.arrayOf(PropTypes.string),
     name: PropTypes.string,
   }),
-  image: PropTypes.bool,
+  image: PropTypes.string,
 };
 
 export default ImageItem;
