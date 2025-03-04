62d1d4c83390f0c060a8522fbb1dea357a7d267c
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-04 16:31:43 +0330

    docs: change cart page for empty state

--- src/Pages/CartPage/index.jsx
+++ src/Pages/CartPage/index.jsx
@@ -22,26 +22,35 @@ const CartPage = () => {
   return (
     <div className="cart __global-container">
       <div className="cart__content">
-        <div className="cart__details">
-          <h1 className="cart__title">Cart list</h1>
-          <h2 className="cart__total--price">Total price: {totalPriceCart}$</h2>
-          <h2 className="cart__total--product">Total products: {numberCart}</h2>
-          <button
-            onClick={() => dispatch(emptyCart())}
-            className="cart__button-all-remove"
-          >
-            Remove All
-          </button>
-        </div>
-        <ul className="cart__list">
-          {listIdCart.length > 0 ? (
-            listIdCart.map((id) => {
-              return <ListItem key={id} id={id} />;
-            })
-          ) : (
-            <h2 className="cart_title-empty">Shopping cart is empty.</h2>
-          )}
-        </ul>
+        {listIdCart.length > 0 ? (
+          listIdCart.map((id) => {
+            return (
+              <>
+                <div className="cart__details">
+                  <h1 className="cart__title">Cart list</h1>
+                  <h2 className="cart__total--price">
+                    Total price: {totalPriceCart}$
+                  </h2>
+                  <h2 className="cart__total--product">
+                    Total products: {numberCart}
+                  </h2>
+                  <button
+                    onClick={() => dispatch(emptyCart())}
+                    className="cart__button-all-remove"
+                  >
+                    Remove All
+                  </button>
+                </div>
+
+                <ul className="cart__list">
+                  <ListItem key={id} id={id} />
+                </ul>
+              </>
+            );
+          })
+        ) : (
+          <h2 className="cart_title-empty">Shopping cart is empty.</h2>
+        )}
       </div>
     </div>
   );
